cdf4fd06269e11eff20635f08ec4b608deb4e3ee
Fernando Unger <fdasilvaunger@gmail.com>
2022-03-16 20:29:17 -0300

    feat: Atualização painel atendente

--- php/attendantpanel.php
+++ php/attendantpanel.php
@@ -3,11 +3,13 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Painel de Controle | Atendente</title>
     <link rel="stylesheet" href="../assets/css/root.css">
     <link rel="stylesheet" href="../assets/css/attendantpanel.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="../assets/img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../assets/img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../assets/img/favicon/favicon-16x16.png">
     <script src="https://kit.fontawesome.com/33abab1032.js" crossorigin="anonymous"></script>
 </head>
 
@@ -17,7 +19,7 @@
             <div class="sidebar-container">
                 <div>
                     <img src="../../assets/img/logo/petmania-black-153.png" alt="Logomarca PetMania" height="105">
-                    <h1>Atendente<br><span>Fernando Unger</span></h1>
+                    <h1>Atendente<br><span>Nome Aqui</span></h1>
                 </div>
                 <div class="sidebar-content">
                     <button class="sidebar__button btn-client" role="button"><i class="fa-solid fa-user-pen"></i>
@@ -37,38 +39,42 @@
                 </div>
                 <div class="main-table">
                     <div class="form-cliente" id="form-client" style="visibility: hidden;">
-
+                        <form action="" method="post">
+                                <button type="submit" class="btn-client btn">Cadastrar Cliente</button>
+                        </form>
                     </div>
                     <div class="form-consulta" style="visibility: hidden;">
-
+                        <form action="" method="post">
+                                <button type="submit" class="btn-consult btn">Cadastrar Consulta</button>
+                        </form>
                     </div>
                     <div class="table-pacient">
                         <table class="table">
                                 <thead id="thead">
                                   <tr>
-                                    <th>Cliente</th>
-                                    <th>Paciente</th>
+                                    <th>Dono</th>
+                                    <th>Espécie</th>
                                     <th>Raça</th>
                                     <th>Informações</th>
                                   </tr>
                                 </thead>
                                 <tbody>
                                   <tr>
-                                    <th>Arthur</th>
-                                    <td>Cachorro</td>
-                                    <td>Golden</td>
+                                  <th>Nome aqui</th>
+                                <td>Nome aqui</td>
+                                <td>Nome aqui</td>
+                                <td>mais informações</td>
+                                  </tr>
+                                  <tr>
+                                    <th>Nome aqui</th>
+                                    <td>Nome aqui</td>
+                                    <td>Nome aqui</td>
                                     <td>mais informações</td>
                                   </tr>
                                   <tr>
-                                    <th>Paulo</th>
-                                    <td>Cachorro</td>
-                                    <td>Uauau</td>
-                                    <td>mais informações</td>
-                                  </tr>
-                                  <tr>
-                                    <th>Fernando</th>
-                                    <td>Cachorro</td>
-                                    <td>Scooby Doo</td>
+                                    <th>Nome aqui</th>
+                                    <td>Nome aqui</td>
+                                    <td>Nome aqui</td>
                                     <td>mais informações</td>
                                   </tr>
                                 </tbody>
