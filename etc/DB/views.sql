40b837339049ef86550ed7ecf778cadbc0524576
Gustavo Ferreira <gustavo2002ramos@gmail.com>
2022-03-16 22:31:18 -0300

    feat:arquivo de view de clientes cadastrados & clientes agendados

--- etc/DB/views.sql
+++ etc/DB/views.sql
@@ -1,1 +1,12 @@
-CREATE OR REPLACE VIEW consulta_animal AS
+# ----- VIEW CLIENTES CADASTRADOS ----- #
+CREATE VIEW Tabela_Pacientes_Cadastrados AS
+SELECT dono.Nome Dono, animal.nome Animal, animal.raca FROM animal
+JOIN dono ON animal.id_dono = dono.id_dono;
+# ----- VIEW CLIENTES CADASTRADOS ----- #
+
+# ----- VIEW CLIENTES AGENDADOS ----- #
+CREATE VIEW Tabela_Pacientes_Agendados AS
+SELECT dono.Nome Dono, animal.nome Animal, animal.raca Raça FROM ficha_medica 
+JOIN animal ON ficha_medica.id_animal = animal.id_animal 
+JOIN dono ON animal.id_dono = dono.id_dono;
+# ----- VIEW CLIENTES CADASTRADOS ----- #
