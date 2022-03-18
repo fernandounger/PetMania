2da19477174c5e1a1d2b9f88e24ccc0350de8734
Fernando Unger <fdasilvaunger@gmail.com>
2022-03-18 11:51:55 -0300

    feat: cadastro consulta

---
+++ php/registerconsult.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Painel de Controle | Cadastrar Consulta</title>
+    <link rel="stylesheet" href="../assets/css/root.css">
+    <link rel="stylesheet" href="../assets/css/attendantpanel.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="../assets/img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../assets/img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../assets/img/favicon/favicon-16x16.png">
+    <script src="https://kit.fontawesome.com/33abab1032.js" crossorigin="anonymous"></script>
+</head>
+
+<body id="body">
+    <div class="container">
+        <aside class="sidebar">
+            <div class="sidebar-container">
+                <div>
+                    <img src="../assets/img/logo/petmania-black-153.png" alt="Logomarca PetMania" height="105">
+                    <h1>Atendente<br><span>Nome Aqui</span></h1>
+                </div>
+                <div class="sidebar-content">
+                    <a href="./registerclient.php">
+                        <button class="sidebar__button btn-client" role="button" type="button"><i
+                                class="fa-solid fa-user-pen"></i>
+                            Cadastrar Cliente</button>
+                    </a>
+                    <a href="./registeranimal.php">
+                        <button class="sidebar__button btn-client btn-animal" role="button" type="button"><i
+                                class="fa-solid fa-paw"></i>
+                            Cadastrar Animal</button>
+                    </a>
+                    <a href="./registerconsult.php">
+                        <button class="sidebar__button btn-consult" role="button" type="button"><i
+                                class="fa-solid fa-notes-medical"></i>
+                            Cadastrar Consulta</button>
+                    </a>
+                    <a href="./attendantpanel.php">
+                        <button class="sidebar__button  btn-pacient" role="button" type="button"><i
+                                class="fa-solid fa-users"></i>
+                            Pacientes</button>
+                    </a>
+                </div>
+            </div>
+        </aside>
+        <main class="main-container">
+            <div class="main-content">
+                <div class="container__title">
+                    <h1 id="title">Cadastrar Consulta</h1>
+                </div>
+                <div class="main-table">
+                    <div class="form-consulta">
+                        <form action="" method="post">
+                            <div class="form-container">
+                                <div class="box">
+                                    <div class="input__animal ipt">
+                                        <label for="animal" class="label">Animal:</label>
+                                        <input type="text" name="idAnimal" class="input" id="animal" value="Scooby" readonly>
+                                    </div>
+                                    <div class="input__data">
+                                        <label for="date" class="label">Data Agendada:</label>
+                                        <input type="date" id="date" name="dataAgendada" class="input" value="18/03/2022" readonly>
+                                    </div>
+                                </div>
+                                <div class="box">
+                                    <div class="ipt">
+                                        <label for="veterinario" class="label">Veterinário:</label>
+                                        <input type="text" name="idVeterinario" class="input" id="veterinario" value="Joselito" readonly>
+                                    </div>
+                                    <div class="ipt">
+                                        <label for="select" class="label">Serviços:</label>
+                                        <select name="select" id="select" autofocus>
+                                            <option value="" selected>Escolha um serviço</option>
+                                            <option value="valor1">Valor 1</option>
+                                            <option value="valor2">Valor 2</option>
+                                            <option value="valor3">Valor 3</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <button type="submit" class="cad-consult btn">Cadastrar Consulta</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+</body>
+
+</html>
