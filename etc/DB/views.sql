411a3515d5c1115a57eca71ece3258db651d79dc
Gustavo Ferreira <gustavo2002ramos@gmail.com>
2022-03-18 20:40:46 -0300

    update:alteração do nome das views pacientes

--- etc/DB/views.sql
+++ etc/DB/views.sql
@@ -1,7 +1,3 @@
-# ----- VIEW CLIENTES CADASTRADOS ----- #
-CREATE VIEW Tabela_Pacientes_Cadastrados AS
-SELECT dono.Nome Dono, animal.nome Animal, animal.raca FROM animal
-JOIN dono ON animal.id_dono = dono.id_dono;
 
 
 /*-----View com tudo do Cliente------*/
@@ -34,12 +30,12 @@ ON dono.id_dono = animal.id_dono;
 
 
 # ----- VIEW CLIENTES CADASTRADOS ----- #
+CREATE VIEW Pacientes_Cadastrados AS
+SELECT dono.Nome Dono, animal.nome Animal, animal.raca FROM animal
+JOIN dono ON animal.id_dono = dono.id_dono;
 
 # ----- VIEW CLIENTES AGENDADOS ----- #
-CREATE VIEW Tabela_Pacientes_Agendados AS
+CREATE VIEW Pacientes_Agendados AS
 SELECT dono.Nome Dono, animal.nome Animal, animal.raca Raça FROM ficha_medica 
 JOIN animal ON ficha_medica.id_animal = animal.id_animal 
 JOIN dono ON animal.id_dono = dono.id_dono;
-
-
-# ----- VIEW CLIENTES CADASTRADOS ----- #
