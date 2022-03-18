6c2119795eae208b202964505ed126b883eb14df
Rafael Moura <rafael.g.moura@hotmail.com>
2022-03-18 20:07:44 -0300

    feat: criacao de view com dados do cliente

--- etc/DB/views.sql
+++ etc/DB/views.sql
@@ -2,6 +2,37 @@
 CREATE VIEW Tabela_Pacientes_Cadastrados AS
 SELECT dono.Nome Dono, animal.nome Animal, animal.raca FROM animal
 JOIN dono ON animal.id_dono = dono.id_dono;
+
+
+/*-----View com tudo do Cliente------*/
+CREATE VIEW tudo_dono AS
+
+SELECT
+dono.id_dono,
+dono.cpf,
+dono.Nome,
+dono.email,
+dono.endereco,
+dono.telefone,
+
+endereco.id_end,
+endereco.cidade,
+endereco.bairro,
+endereco.logradouro,
+endereco.numero,
+endereco.CEP,
+
+animal.id_animal,
+animal.nome nome_animal
+
+FROM dono 
+LEFT JOIN endereco
+ON dono.endereco = endereco.id_end
+RIGHT JOIN animal
+ON dono.id_dono = animal.id_dono;
+
+
+
 # ----- VIEW CLIENTES CADASTRADOS ----- #
 
 # ----- VIEW CLIENTES AGENDADOS ----- #
@@ -9,4 +40,6 @@ CREATE VIEW Tabela_Pacientes_Agendados AS
 SELECT dono.Nome Dono, animal.nome Animal, animal.raca Raça FROM ficha_medica 
 JOIN animal ON ficha_medica.id_animal = animal.id_animal 
 JOIN dono ON animal.id_dono = dono.id_dono;
+
+
 # ----- VIEW CLIENTES CADASTRADOS ----- #
