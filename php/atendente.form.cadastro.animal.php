2e34923736efbf9f365c883c8c30f5f914bde115
Rafael Moura <rafael.g.moura@hotmail.com>
2022-03-20 16:03:33 -0300

    feat: inserindo menu em um include

--- php/atendente.form.cadastro.animal.php
+++ php/atendente.form.cadastro.animal.php
@@ -16,30 +16,9 @@
 
 <body id="body">
     <div class="container">
-        <aside class="sidebar">
-            <div class="sidebar-container">
-                <div>
-                    <img src="../assets/img/logo/petmania-black-153.png" alt="Logomarca PetMania" height="105">
-                    <h1>Atendente<br><span>Nome Aqui</span></h1>
-                </div>
-                <div class="sidebar-content">
-                    <a href="./redirect.cliente.listar.php">
-                    <button class="sidebar__button btn-client" role="button" type="button"><i
-                                class="fa-solid fa-user-pen"></i>
-                             Clientes</button>
-                    </a>
-                    <a href="./atendente.listar.animal.php">
-                        <button class="sidebar__button btn-client btn-animal" role="button" type="button"><i class="fa-solid fa-paw"></i>
-                             Animais</button>
-                    </a>
-                    <a href="./atendente.listar.consulta.php">
-                        <button class="sidebar__button btn-consult" role="button" type="button"><i
-                                class="fa-solid fa-notes-medical"></i>
-                             Consultas</button>
-                    </a>
-                </div>
-            </div>
-        </aside>
+        <?php
+            include('./menu.php');
+        ?>
         <main class="main-container">
             <div class="main-content">
                 <div class="container__title">
