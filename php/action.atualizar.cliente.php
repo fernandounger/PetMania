fc3fc6bbb9866284609bd3c4cf4359958c9a47d8
Rafael Moura <rafael.g.moura@hotmail.com>
2022-03-20 14:58:59 -0300

    feat: editar cadastro de clientes

---
+++ php/action.atualizar.cliente.php
@@ -0,0 +1,40 @@
+<?php
+
+require('./atendente.crud.php');
+
+
+if(
+    !empty($_POST['nome_cliente']) ||
+    !empty($_POST['cpf']) ||
+    !empty($_POST['email']) ||
+    !empty($_POST['telefone']) ||
+    !empty($_POST['cidade']) ||
+    !empty($_POST['bairro']) ||
+    !empty($_POST['logradouro']) ||
+    !empty($_POST['cep']) ||
+    !empty($_POST['numero']) ||
+    !empty($_POST['id_cliente'])
+    
+)
+{
+    $cliente = new stdClass();
+    $cliente->Nome = $_POST['nome_cliente'];
+    $cliente->cpf = $_POST['cpf'];
+    $cliente->email = $_POST['email'];
+    $cliente->telefone = $_POST['telefone'];
+    $cliente->cidade = $_POST['cidade'];
+    $cliente->bairro = $_POST['bairro'];
+    $cliente->logradouro = $_POST['logradouro'];
+    $cliente->CEP = $_POST['cep'];
+    $cliente->numero = $_POST['numero'];
+    $cliente->id_dono = $_POST['id_cliente'];
+    $cliente->endereco = $_POST['id_end'];
+
+
+    if(AtualizaCliente($cliente)){
+        header('location: atendente.listar.cliente.php?status=sucess');
+    }else{
+        header('location: atendente.listar.cliente.php?status=fail');
+    }
+    
+}
--- php/atendente.form.edit.cliente.php
+++ php/atendente.form.edit.cliente.php
@@ -1,3 +1,4 @@
+<?php session_start()?>
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -46,27 +47,31 @@
                 </div>
                 <div class="main-table">
                     <div class="form-cliente">
-                        <form action="cliente.listar.redirect.php" method="post">
+                        <form action="./action.atualizar.cliente.php" method="post">
                             <div class="form-container">
                                 <div class="cliente__box">
                                     <div class="ipt">
                                         <label for="cliente" class="label">Nome:</label>
                                         <input type="text" name="nome_cliente" class="input" id="nome"
+                                        value="<?= $_SESSION['atualiza_cliente']->Nome ?>"
                                             placeholder="Digite Seu Nome">
                                     </div>
                                     <div class="ipt">
                                         <label for="cpf" class="label">CPF:</label>
                                         <input type="text" name="cpf" class="input" id="cpf"
+                                        value="<?= $_SESSION['atualiza_cliente']->cpf ?>"
                                             placeholder="000.000.000-00">
                                     </div>
                                     <div class="ipt">
                                         <label for="email" class="label">Email:</label>
                                         <input type="text" name="email" class="input" id="email"
+                                        value="<?= $_SESSION['atualiza_cliente']->email ?>"
                                             placeholder="Digite Seu Email">
                                     </div>
                                     <div class="ipt">
                                         <label for="telefone" class="label">Telefone:</label>
                                         <input type="text" name="telefone" class="input" id="telefone"
+                                        value="<?= $_SESSION['atualiza_cliente']->telefone ?>"
                                             placeholder="(00) 00000-0000">
                                     </div>
                                 </div>
@@ -75,30 +80,37 @@
                                     <div class="ipt">
                                         <label for="cidade" class="label">Cidade:</label>
                                         <input type="text" name="cidade" class="input" id="cidade"
+                                        value="<?= $_SESSION['atualiza_cliente']->cidade ?>"
                                             placeholder="Digite o Nome da Sua Cidade">
                                     </div>
                                     <div class="ipt">
                                         <label for="bairro" class="label">Bairro:</label>
                                         <input type="text" name="bairro" class="input" id="bairro"
+                                        value="<?= $_SESSION['atualiza_cliente']->bairro ?>"
                                             placeholder="Digite o Nome da Seu Bairro">
                                     </div>
                                     <div class="ipt">
                                         <label for="logradouro" class="label">Logradouro:</label>
                                         <input type="text" name="logradouro" class="input" id="logradouro"
+                                        value="<?= $_SESSION['atualiza_cliente']->logradouro ?>"
                                             placeholder="Digite o Seu Logradouro">
                                     </div>
                                     <div class="">
                                         <label for="cep" class="label">CEP:</label>
-                                        <input type="text" name="cep" class="input" id="cep" placeholder="00000-000">
+                                        <input type="text" name="cep" class="input" id="cep"
+                                        value="<?= $_SESSION['atualiza_cliente']->numero ?>" placeholder="00000-000">
                                     </div>
                                     <div>
                                         <label for="numero" class="label">Número:</label>
                                         <input type="text" name="numero" class="input input-n" id="numero"
+                                        value="<?= $_SESSION['atualiza_cliente']->CEP ?>"
                                             placeholder="N°">
                                     </div>
-
+                                    <input type="hidden" id="id_end" name="id_end" value="<?= $_SESSION['atualiza_cliente']->endereco ?>">
                                 </div>
-                                <button type="submit" class="cad-cliente btn">Editar Cliente</button> 
+                                <button type="submit" class="cad-cliente btn"
+                                name="id_cliente" value="<?= $_SESSION['atualiza_cliente']->id_dono ?>"
+                                >Editar Cliente</button> 
                             </div>
                     </div>
                     </form>
