27c8bd9e83163a0657db0e6e71b7d983e08604ad
Rafael Moura <rafael.g.moura@hotmail.com>
2022-03-20 22:41:17 -0300

    feat: action cadastro de consultas

---
+++ php/action.cadastrar.consulta.php
@@ -0,0 +1,26 @@
+<?php
+
+require('./atendente.crud.php');
+
+
+if(
+    empty($_POST['idAnimal']) ||
+    empty($_POST['idVet']) ||
+    empty($_POST['motivo'])
+){
+    echo "erro: faltou algo!";
+    
+    header("Refresh: 10; atendente.form.cadastro.consulta.php");
+}
+
+else{
+    $consulta = new stdClass();
+    $consulta->idAnimal = $_POST['idAnimal'];
+    $consulta->idVet = $_POST['idVet'];
+    $consulta->motivo = $_POST['motivo'];
+
+    if(CadastroFichaMedica($consulta)){
+        header("location: atendente.listar.consulta.php");
+    }
+
+}
