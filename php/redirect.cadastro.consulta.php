d907a2d981a9785af176f06047c07b3db521dc2d
Rafael Moura <rafael.g.moura@hotmail.com>
2022-03-20 21:06:42 -0300

    fix: correcao no redirect para cadastro

--- php/redirect.cadastro.consulta.php
+++ php/redirect.cadastro.consulta.php
@@ -1,27 +1,13 @@
 <?php
+
 require('./atendente.crud.php');
+
 session_start();
 
-if(isset($_GET['id_animal']) && !empty($_GET['id_animal']) && $_GET['id_animal'] != NULL)
+if(isset($_GET['idAnimal']) && !empty($_GET['idAnimal']) && $_GET['idAnimal'] != NULL)
 {
-    $_SESSION['listamedico'] = listarVeterinarios();
+    $_SESSION['listamedicos'] = listarVeterinarios();
+    
+    header("location: atendente.form.cadastro.consulta.php?idAnimal={$_GET['idAnimal']}&nomeAnimal={$_GET['nomeAnimal']}");
     
 }
-
-var_dump($_SESSION['listamedico']);
-
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
-
-echo $_GET['id_animal'];
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
-
-echo $_GET['nome_animal'];
-
-/*
-header("location: ./atendente.listar.animal.php?id_animal={$_GET['id_animal']}");
