299457e69acdc253370523cafd1f99ed58c84114
Gustavo Ramos <83662146+GusTaVp-coder@users.noreply.github.com>
2022-03-21 17:14:34 -0300

    rm: span 'nome'

--- php/menu.veterinario.php
+++ php/menu.veterinario.php
@@ -4,7 +4,7 @@
                     <a href="index.php">
                     <img src="../assets/img/logo/petmania-black-153.png" alt="Logomarca PetMania" height="105">
                     </a>
-                    <h1>Veterinário<br><span>Nome Aqui</span></h1>
+                    <h1>Veterinário</h1>
                 </div>
                 <div class="sidebar-content">
                     <a href="redirect.veterinario.listar.consulta.php"><button class="sidebar__button btn-consult" role="button"><i class="fa-solid fa-file-medical"></i>
